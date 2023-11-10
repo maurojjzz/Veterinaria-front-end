d51c9668811415db57af62bb7fa080759831326c
Mauro Jimenez <maurojim123@gmail.com>
2023-11-10 12:20:20 -0300

    Admin usuario view

--- src/Components/Cliente/Form/index.jsx
+++ src/Components/Cliente/Form/index.jsx
@@ -1,33 +1,99 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Input } from "../../Shared";
-import styles from './form.module.css';
-import {usuarioSchema}  from "../../../Validations";
-import { joiResolver } from '@hookform/resolvers/joi';
-
+import styles from "./form.module.css";
+import { usuarioSchema } from "../../../Validations";
+import { joiResolver } from "@hookform/resolvers/joi";
 
 const FormClient = () => {
-  const { register, handleSubmit, formState:{errors} } = useForm({
-    mode: 'onBlur',
-    resolver: joiResolver(usuarioSchema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+    resolver: joiResolver(usuarioSchema),
   });
 
-  const onSubmit = (data)=>{
-    console.log(data)
-    console.log(errors)
-  }
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log(errors);
+  };
 
   return (
     <div className={`flex-grow-1 d-flex flex-column align-items-center justify-content-center py-5 `}>
-      <form className={`container d-flex flex-column p-4 ${styles.formContainer} `} onSubmit={handleSubmit(onSubmit)}>
-        <Input labelText={`Nombre`} placeholder={`Lionel`} type={`text`} name={'nombre'} register={register}/>
-        <Input labelText={`Apellido`} placeholder={`Messi`} type={`text`} name={'apellido'} register={register}/>
-        <Input labelText={`Email`} placeholder={`liomessigamer@gmail.com`} type={`email`} name={'email'} register={register}/>
-        <Input labelText={`Direccion`} placeholder={`Alberdi 4250`} type={`text`} name={'direccion'} register={register}/>
-        <Input labelText={`Nro Documento`} placeholder={`42168754`} type={`text`} name={'nro_doc'} register={register}/>
-        <Input labelText={`Telefono`} placeholder={`+54 3416598754`} type={`text`} name={'telefono'} register={register}/>
-        <Input labelText={`Contraseña`} placeholder={`Contraseña123`} type={`Password`} name={'password'} register={register}/>
-        <button className="btn btn-primary" type='submit'>Enviar</button>
+      <form
+        className={`container d-flex flex-column align-items-center p-4 pt-5 rounded-3 ${styles.formContainer} `}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}>
+          <Input
+            labelText={`Nombre`}
+            placeholder={`Lionel`}
+            type={`text`}
+            name={"nombre"}
+            register={register}
+            error={errors.nombre?.message}
+          />
+          <Input
+            labelText={`Apellido`}
+            placeholder={`Messi`}
+            type={`text`}
+            name={"apellido"}
+            register={register}
+            error={errors.apellido?.message}
+          />
+        </div>
+
+        <div className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}>
+          <Input
+            labelText={`Email`}
+            placeholder={`liomessigamer@gmail.com`}
+            type={`email`}
+            name={"email"}
+            register={register}
+            error={errors.email?.message}
+          />
+          <Input
+            labelText={`Direccion`}
+            placeholder={`Alberdi 4250`}
+            type={`text`}
+            name={"direccion"}
+            register={register}
+            error={errors.direccion?.message}
+          />
+        </div>
+        <div className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}>
+          <Input
+            labelText={`Nro Documento`}
+            placeholder={`42168754`}
+            type={`text`}
+            name={"nro_doc"}
+            register={register}
+            error={errors.nro_doc?.message}
+          />
+          <Input
+            labelText={`Telefono`}
+            placeholder={`+54 3416598754`}
+            type={`text`}
+            name={"telefono"}
+            register={register}
+            error={errors.telefono?.message}
+          />
+        </div>
+        <div className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}>
+          <Input
+            labelText={`Contraseña`}
+            placeholder={`Contraseña123`}
+            type={`Password`}
+            name={"password"}
+            register={register}
+            error={errors.password?.message}
+          />
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Enviar
+        </button>
       </form>
     </div>
   );
--- src/Components/Shared/Input/index.jsx
+++ src/Components/Shared/Input/index.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import styles from "./input.module.css";
 
-const Input = ({ labelText, placeholder, type, register, name }) => {
+const Input = ({ labelText, placeholder, type, register, name, error }) => {
   return (
-    <div className={`form-floating mb-3  ${styles.goodCont}`}>
+    <div className={`d-flex flex-column form-floating mb-3 ${styles.goodCont}`}>
       <input
         type={type}
-        className={`form-control ${styles.formInput} `}
+        className={!error ? `form-control ${styles.formInput} ` : `form-control is-invalid ${styles.formInput} ${styles.formInputError}`}
         id={`floatingInput-${labelText}`}
         placeholder={placeholder}
         autoComplete="off"
         {...register(name, { required: { value: true, message: "Este campo es requerido" } })}
       />
-      <label className={` ${styles.formLoginLabel} `} htmlFor={`floatingInput-${labelText}`}>
+      <label className={!error ? ` ${styles.formLoginLabel} text-info ` : `text-danger`} htmlFor={`floatingInput-${labelText}`}>
         {labelText}
       </label>
+      {error ? (
+        <div className={`${styles.errorContainer}`}>
+          <div className={`text-center  ${styles.errorMensaje}`}>{error}</div> 
+        </div>
+      ) : (
+        <div className={`${styles.spaceErrorMsg}`}></div>
+      )}
     </div>
-  );
-};
+  )
+}
 
 export default Input;
