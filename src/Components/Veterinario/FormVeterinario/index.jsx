c0b8eb92eafb08ce265c30108e376ae56109a4e2
Nicolás <nicorossetti09@hotmail.com>
2023-11-12 22:34:09 -0300

    Correccion Validaciones

--- src/Components/Veterinario/FormVeterinario/index.jsx
+++ src/Components/Veterinario/FormVeterinario/index.jsx
@@ -1,43 +1,183 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Input } from "../../Shared";
-import styles from './formVeterinario.module.css';
+import { useHistory, useLocation, useParams } from "react-router-dom";
+import { Input, ButtonSubmit } from "../../Shared";
+import styles from "./formVeterinario.module.css";
 import { veterinarioSchema } from "../../../Validations";
-import { joiResolver } from '@hookform/resolvers/joi';
+import { joiResolver } from "@hookform/resolvers/joi";
 
 const FormVeterinario = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    mode: 'onBlur',
-    resolver: joiResolver(veterinarioSchema) 
+  const { id } = useParams();
+  const location = useLocation();
+  const history = useHistory();
+
+  const dataForm = location.state?.params;
+
+  const veterinarioDataUpdate = {
+    matricula: dataForm?.matricula,
+    nombre: dataForm?.nombre,
+    apellido: dataForm?.apellido,
+    telefono: dataForm?.telefono,
+    email: dataForm?.email,
+    nro_doc: dataForm?.nro_doc,
+    rol: dataForm?.rol || "65334d8d48ec52ff5e08c85a",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+    resolver: joiResolver(veterinarioSchema),
+    defaultValues: {
+      ...veterinarioDataUpdate,
+    },
   });
 
+  const goBackToTable = () => {
+    setTimeout(() => {
+      history.push("/veterinarios");
+    }, 2000);
+  };
+
+  const addVeterinario = async (data) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_KEY}/veterinarios`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        console.log("Se creó correctamente");
+        goBackToTable();
+      } else {
+        console.log("No se pudo crear el veterinario");
+      }
+    } catch (error) {
+      console.error("Error al crear veterinario", error);
+    }
+  };
+
+  const updateVeterinario = async (data) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_KEY}/veterinarios/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        console.log("Se actualizó correctamente");
+        goBackToTable();
+      } else {
+        console.log("No se pudo actualizar el veterinario");
+      }
+    } catch (error) {
+      console.error("Error al actualizar veterinario", error);
+    }
+  };
+
   const onSubmit = (data) => {
-    console.log(data);
-    console.log(errors);
+    console.log("Datos", data);
+    if (!id) {
+      addVeterinario(data);
+    } else {
+      updateVeterinario(data);
+    }
   };
 
   return (
     <div className={`flex-grow-1 d-flex flex-column align-items-center justify-content-center py-5 `}>
       <form
         className={`container d-flex flex-column align-items-center p-4 pt-5 rounded-3 ${styles.formContainer} `}
-        onSubmit={handleSubmit(onSubmit)}>
-        <div className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}>
-          <Input labelText={`Matrícula`} placeholder={`123456`} type={`text`} name={'matricula'} register={register} />
-          <Input labelText={`Nombre`} placeholder={`Nombre`} type={`text`} name={'nombre'} register={register} />
-          </div>
-          <div className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}>
-          <Input labelText={`Apellido`} placeholder={`Apellido`} type={`text`} name={'apellido'} register={register} />
-          <Input labelText={`Teléfono`} placeholder={`Teléfono`} type={`text`} name={'telefono'} register={register} />
-          </div>
-          <div className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}>
-          <Input labelText={`Email`} placeholder={`Email`} type={`email`} name={'email'} register={register} />
-          <Input labelText={`Nro Documento`} placeholder={`Nro Documento`} type={`text`} name={'nro_doc'} register={register} />
-          </div>
-          <div className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}>
-          <Input labelText={`Contraseña`} placeholder={`Contraseña`} type={`Password`} name={'password'} register={register} />
-          <Input labelText={`Confirmar Contraseña`} placeholder={`Contraseña`} type={`Password`} name={'password'} register={register} />
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div
+          className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}
+        >
+          <Input
+            labelText={`Matrícula`}
+            placeholder={`123456`}
+            type={`text`}
+            name={"matricula"}
+            register={register}
+            error={errors.matricula?.message}
+          />
+          <Input
+            labelText={`Nombre`}
+            placeholder={`Nombre`}
+            type={`text`}
+            name={"nombre"}
+            register={register}
+            error={errors.nombre?.message}
+          />
         </div>
-        <button className="btn btn-primary" type='submit'>Enviar</button>
+
+        <div
+          className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}
+        >
+          <Input
+            labelText={`Apellido`}
+            placeholder={`Apellido`}
+            type={`text`}
+            name={"apellido"}
+            register={register}
+            error={errors.apellido?.message}
+          />
+          <Input
+            labelText={`Teléfono`}
+            placeholder={`Teléfono`}
+            type={`text`}
+            name={"telefono"}
+            register={register}
+            error={errors.telefono?.message}
+          />
+        </div>
+        <div
+          className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}
+        >
+          <Input
+            labelText={`Email`}
+            placeholder={`Email`}
+            type={`email`}
+            name={"email"}
+            register={register}
+            error={errors.email?.message}
+          />
+          <Input
+            labelText={`Nro Documento`}
+            placeholder={`Nro Documento`}
+            type={`text`}
+            name={"nro_doc"}
+            register={register}
+            error={errors.nro_doc?.message}
+          />
+        </div>
+        <div
+          className={`d-flex flex-column flex-md-row align-items-center justify-content-evenly ${styles.groupInput}`}
+        >
+          <Input
+            labelText={`Contraseña`}
+            placeholder={`Contraseña`}
+            type={`password`}
+            name={"password"}
+            register={register}
+            error={errors.password?.message}
+          />
+          <Input
+            labelText={`Repetir Contraseña`}
+            placeholder={`Repetir Contraseña`}
+            type={`password`}
+            name={`repeatPassword`}
+            register={register}
+            error={errors.repeatPassword?.message}
+          />
+        </div>
+        <ButtonSubmit msg={`ENVIAR`} clickAction={() => {}} type={`submit`} />
       </form>
     </div>
   );
--- src/Validations/veterinarioSchema.js
+++ src/Validations/veterinarioSchema.js
@@ -3,38 +3,42 @@ import Joi from 'joi';
 const veterinarioSchema = Joi.object({
   matricula: Joi.string()
     .required()
+    .length(8)
+    .alphanum()
     .messages({
       "string.base": "Matrícula debe ser una cadena de texto",
       "string.empty": "Este es un campo requerido",
+      "string.length": "La matrícula debe tener exactamente 8 caracteres",
+      "string.alphanum": "La matrícula solo debe contener números y letras",
     }),
 
-  apellido: Joi.string()
-    .min(3)
-    .max(25)
-    .regex(/^[a-zA-Z ]+$/)
-    .messages({
-      "string.base": "Apellido debe ser una cadena de texto",
-      "string.empty": "Este es un campo requerido",
-      "string.min": "Apellido como mínimo 3 caracteres",
-      "string.max": "Apellido como máximo 25 caracteres",
-      "string.pattern.base": "Apellido solo debe contener letras",
-    })
-    .required(),
-
-  nombre: Joi.string()
+    nombre: Joi.string()
     .min(3)
     .max(25)
     .regex(/^[a-zA-Z ]+$/)
     .messages({
       "string.base": "Nombre debe ser una cadena de texto",
       "string.empty": "Este es un campo requerido",
-      "string.min": "Nombre como mínimo 3 caracteres",
-      "string.max": "Nombre como máximo 25 caracteres",
+      "string.min": "Nombre como minimo 3 caracteres",
+      "string.max": "Nombre como maximo 25 caracteres",
       "string.pattern.base": "Nombre solo debe contener letras",
     })
     .required(),
 
-  telefono: Joi.string()
+    apellido: Joi.string()
+    .min(3)
+    .max(25)
+    .regex(/^[a-zA-Z ]+$/)
+    .messages({
+      "string.base": "Apellido debe ser una cadena de texto",
+      "string.empty": "Este es un campo requerido",
+      "string.min": "Apellido como minimo 3 caracteres",
+      "string.max": "Apellido como maximo 25 caracteres",
+      "string.pattern.base": "Apellido solo debe contener letras",
+    })
+    .required(),
+
+    telefono: Joi.string()
     .pattern(/^\+?[0-9]{9,15}$/)
     .required()
     .messages({
@@ -43,25 +47,42 @@ const veterinarioSchema = Joi.object({
       "string.pattern.base": "El número de teléfono debe contener entre 9 y 15 dígitos numéricos",
     }),
 
-  email: Joi.string().email({ tlds: { allow: false } }).required().messages({
-    "string.base": "Email debe ser una cadena de texto",
-    "string.empty": "Este es un campo requerido",
-    "string.email": "Debe ser un email válido",
-  }),
+    email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      "string.base": "email debe ser una cadena de texto",
+      "string.empty": "Este campo no puede estar vacio",
+      "string.email": "Debe ser un email valido",
+    }),
 
-  password: Joi.string()
+    nro_doc: Joi.string()
+    .pattern(/^[0-9]{7,8}$/)
+    .required()
+    .messages({
+      "string.base": "El DNI debe ser una cadena de texto",
+      "string.empty": "Este es un campo requerido",
+      "string.pattern.base": "El DNI debe contener entre 7 y 8 dígitos numéricos",
+    }),
+
+    password: Joi.string()
     .min(8)
     .max(25)
     .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)
     .required()
     .messages({
       "string.base": "La contraseña debe ser una cadena de texto",
-      "string.empty": "Este es un campo requerido",
+      "string.empty": "La contraseña es un campo requerido",
       "string.min": "La contraseña debe tener al menos {#limit} caracteres",
       "string.max": "La contraseña no debe tener más de {#limit} caracteres",
       "string.pattern.base":
-        "La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número",
+      "La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número",
     }),
+  repeatPassword: Joi.string().valid(Joi.ref("password")).required().messages({
+    "string.base": "La contraseña debe ser una cadena de texto",
+    "string.empty": "La contraseña es un campo requerido",
+    "any.only": "Las contraseñas no son iguales"
+  }),
 });
 
 export default veterinarioSchema;
