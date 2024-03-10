77bafaab9ce8c0801afb8ad71e1b34394bdddb72
maurojjzz <maurojim123@gmail.com>
2024-03-10 20:18:01 -0300

    icon de usuario logged

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import styles from "./header.module.css";
-import {useHistory} from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import LoggedInfo from "../Shared/LoggedInfo";
 
-
-const Header = ({setClickHamb, clickHamb}) => {
-
+const Header = ({ setClickHamb, clickHamb }) => {
   const history = useHistory();
+  const token = localStorage.getItem("token");
 
   return (
     <div className={`d-flex justify-content-evenly align-items-center text-light ${styles.headerContainer}`}>
-      <div onClick={()=>{setClickHamb(!clickHamb)}}>
+      <div
+        onClick={() => {
+          setClickHamb(!clickHamb);
+        }}
+      >
         <img
           src={`${process.env.PUBLIC_URL}/assets/icons/menu.png`}
           alt={`hamburguer menu icon`}
           className={`d-md-none ${styles.logo}`}
         />
       </div>
-      <div onClick={()=>{history.push('/')}} className={`d-flex gap-2 align-items-center ${styles.logotitle}`}>
+      <div
+        onClick={() => {
+          history.push("/");
+        }}
+        className={`d-flex gap-2 align-items-center ${styles.logotitle}`}
+      >
         <img
           src={`${process.env.PUBLIC_URL}/assets/icons/pata.png`}
           alt={`dog footprint logo`}
@@ -24,20 +33,38 @@ const Header = ({setClickHamb, clickHamb}) => {
         />
         <h1 className={`d-none d-sm-block`}>VeterinariAPP</h1>
       </div>
-      <img
-        src={`${process.env.PUBLIC_URL}/assets/icons/dog-login.png`}
-        alt={`dog entering a door simulating login icon`}
-        className={`d-md-none ${styles.logo}`}
-        onClick={()=>{history.push('/auth/login')}}
-      />
-      <div className={`d-none d-md-flex gap-5`}>
-        <div className={`${styles.linkAccount}`} onClick={()=>{history.push('/auth/login')}}>
-          <h2>Login</h2>
-        </div>
-        <div className={`${styles.linkAccount}`} onClick={()=>{history.push('/auth/sign-up')}}>
-          <h2>Sign Up</h2>
-        </div>
-      </div>
+      {!token ? (
+        <>
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/icons/dog-login.png`}
+            alt={`dog entering a door simulating login icon`}
+            className={`d-md-none ${styles.logo}`}
+            onClick={() => {
+              history.push("/auth/login");
+            }}
+          />
+          <div className={`d-none d-md-flex gap-5`}>
+            <div
+              className={`${styles.linkAccount}`}
+              onClick={() => {
+                history.push("/auth/login");
+              }}
+            >
+              <h2>Login</h2>
+            </div>
+            <div
+              className={`${styles.linkAccount}`}
+              onClick={() => {
+                history.push("/auth/sign-up");
+              }}
+            >
+              <h2>Sign Up</h2>
+            </div>
+          </div>
+        </>
+      ) : (
+        <LoggedInfo />
+      )}
     </div>
   );
 };
