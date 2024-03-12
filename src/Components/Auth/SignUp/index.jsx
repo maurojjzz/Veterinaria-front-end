7b510dbc2485214aaaaf4074d80f4b09190f7a77
maurojjzz <maurojim123@gmail.com>
2024-03-12 20:50:53 -0300

    Toast alert design

--- src/Components/Auth/SignUp/index.jsx
+++ src/Components/Auth/SignUp/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './signup.module.css'
+import Toast from '../../Shared/Toast'
 
 const SignUp = () => {
   return (
     <div>
       <h1>Sign Up en desarrollo</h1>
+      <Toast title={"Error"} message={"Error al loguearse"} />
+      <Toast title={"Success"} message={"Error al loguearse"} />
+      <Toast title={"Warning"} message={"Error al loguearse"} />
+      <Toast title={"Info"} message={"Error al loguearse"} />
     </div>
   )
 }
