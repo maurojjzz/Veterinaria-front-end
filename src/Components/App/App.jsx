7c2e8dc3d69348a054e612b795d4985ee08fc29e
Miguel58000 <miguelrodriguezips36@gmail.com>
2024-11-27 16:52:49 -0300

    Nuevas modificaciones a la página Home

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Footer from './components/Footer'; //por alguna razón que no logro detectar, no lo está tomando//
-import './styles.css';
+import React from "react";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Hero />
-      <About/>
-      <Footer/>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className={styles.container}>
+            <main className={styles.mainContent}>
+                <Home />
+            </main>
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
+
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -4,6 +4,13 @@ import styles from './home.module.css';
 const Home = () => {
   return (
     <section id="hero" className={styles.heroSection}>
+            <div>
+              <img
+            src={`${process.env.PUBLIC_URL}/assets/images/Veterinary-bro.png`}
+            alt={`Veterinario atendiendo a un perrito`}
+            className={`img-fluid ${styles.heroImage} border-danger`}
+          />
+            </div>
       <div className={styles.heroContent}>
         <h2 className={styles.heroTitle}>
           Cuidamos a tus mascotas como parte de nuestra familia
@@ -15,11 +22,7 @@ const Home = () => {
           <button className={styles.btnPrimary}>Ver Servicios</button>
           <button className={styles.btnSecondary}>Agendar Cita</button>
         </div>
-        <img
-            src={`${process.env.PUBLIC_URL}/assets/images/Veterinary-bro.png`}
-            alt={`Veterinario atendiendo a un perrito`}
-            className={`img-fluid ${styles.heroImage} border-danger`}
-          />
+
       </div>
     </section>
   );
