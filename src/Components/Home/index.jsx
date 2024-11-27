9e3993e62961eae8e99ce4b7d2147f3b38d372cb
maurojjzz <maurojim123@gmail.com>
2024-11-27 19:34:41 -0300

    resolviendo errores footer

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import styles from './home.module.css';
+import React from "react";
+import styles from "./home.module.css";
+import Footer from "../Footer";
 
 const Home = () => {
   return (
-    <section id="hero" className={styles.heroSection}>
-            <div>
-              <img
+    <div>
+      <div id="hero" className={`position-relative ${styles.heroSection}`}>
+        <div>
+          <img
             src={`${process.env.PUBLIC_URL}/assets/images/Veterinary-bro.png`}
             alt={`Veterinario atendiendo a un perrito`}
             className={`img-fluid ${styles.heroImage} border-danger`}
           />
-            </div>
-      <div className={styles.heroContent}>
-        <h2 className={styles.heroTitle}>
-          Cuidamos a tus mascotas como parte de nuestra familia
-        </h2>
-        <p className={styles.heroText}>
-          Servicios veterinarios de calidad con atención personalizada.
-        </p>
-        <div className={styles.heroButtons}>
-          <button className={styles.btnPrimary}>Ver Servicios</button>
-          <button className={styles.btnSecondary}>Agendar Cita</button>
         </div>
-
+        <div className={styles.heroContent}>
+          <h2 className={styles.heroTitle}>Cuidamos a tus mascotas como parte de nuestra familia</h2>
+          <p className={styles.heroText}>Servicios veterinarios de calidad con atención personalizada.</p>
+          <div className={styles.heroButtons}>
+            <button className={styles.btnPrimary}>Ver Servicios</button>
+            <button className={styles.btnSecondary}>Agendar Cita</button>
+          </div>
+        </div>
       </div>
-    </section>
+      <Footer />
+    </div>
   );
 };
 
--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -3,8 +3,6 @@ import styles from "./layout.module.css";
 import Header from "../Header";
 import MenuHamburguesa from "../MenuHamburguesa";
 import SideBar from "../SideBar";
-import Home from "../Home";
-import Footer from "../Footer";
 
 const Layout = (props) => {
   const [clickHamb, setClickHamb] = useState(false);
@@ -12,16 +10,15 @@ const Layout = (props) => {
   return (
     <div className={`container-fluid d-flex flex-column p-0 ${styles.whole_layout_cont}`}>
       <Header setClickHamb={setClickHamb} clickHamb={clickHamb} />
-      <div className="d-sm-block d-md-none">{clickHamb && <MenuHamburguesa routes={props.routes} setClickHamb={setClickHamb} />}</div>
+      <div className="d-sm-block d-md-none">
+        {clickHamb && <MenuHamburguesa routes={props.routes} setClickHamb={setClickHamb} />}
+      </div>
       <div className="d-flex flex-grow-1">
         <div className="d-none d-md-block z-1">
-          <SideBar routes={props.routes} setClickHamb={setClickHamb}/>
+          <SideBar routes={props.routes} setClickHamb={setClickHamb} />
         </div>
-        <div className={`position-absolute w-100 h-100 ${styles.contentChild}`}>
-          {props.children}
-        </div>
+        <div className={`position-absolute w-100 h-100 ${styles.contentChild}`}>{props.children}</div>
       </div>
-      <Footer/>
     </div>
   );
 };
