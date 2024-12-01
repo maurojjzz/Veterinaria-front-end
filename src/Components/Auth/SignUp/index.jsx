606bb940e061a9057451ee5099643ee2b225cf1f
maurojjzz <maurojim123@gmail.com>
2024-12-01 13:49:04 -0300

    modal confirmacion hecho

--- src/Components/Auth/SignUp/index.jsx
+++ src/Components/Auth/SignUp/index.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import styles from './signup.module.css'
-import Toast from '../../Shared/Toast'
+import { Toast, ModalAlert } from "../../Shared";
 
 const SignUp = () => {
-  const [error, setError] = useState(true)
+  const [error, setError] = useState(true);
+
+
+
+
   return (
     <div>
       <h1>Sign Up en desarrollo</h1>
-      {error && <Toast title={"Error"} message={"Error al loguearse"} setError={setError} />}
+      {error && <Toast title={"Info"} message={"Error al loguearse"} setError={setError} />}
+
+      <ModalAlert text={"Prbando"} clickAction={() => setError(false)} />
 
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
