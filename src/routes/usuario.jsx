2538be6af5f1b9930f3f0e375d9b67b8903f7f50
Miguel58000 <miguelrodriguezips36@gmail.com>
2024-12-14 17:02:40 -0300

    Atenciones pendientes de pago v1.01

--- src/routes/usuario.jsx
+++ src/routes/usuario.jsx
@@ -4,31 +4,38 @@ import Layout from '../Components/Layout';
 import DasboardUser from '../Components/Users/User';
 import NotAllowed from '../Components/Auth/Not-Allowed';
 import Atencion from '../Components/Atencion';
-
+import AtencionesPendientesPago from '../Components/AtencionesPendientesPago';
 
 const UsuarioRoutes = () => {
-  const { url } = useRouteMatch()
+  const { url } = useRouteMatch();
 
   const routes = [
     {
       name: 'Home',
       path: '/user/dash',
       image: 'home.png',
-      alt: 'home icon'  
+      alt: 'home icon',
     },
     {
       name: 'Atenciones',
       path: '/user/atencion',
       image: 'order-history.png',
-      alt: 'atenciones icon'  
+      alt: 'atenciones icon',
+    },
+    {
+      name: 'Atenciones Pendientes de Pago',
+      path: '/user/atenciones-pendientes',
+      image: 'pending-payment.png',
+      alt: 'pending payments icon',
     },
   ];
-  
+
   return (
     <Layout routes={routes}>
       <Switch>
         <Route exact path={`${url}/dash`} component={DasboardUser} />
         <Route exact path={`${url}/atencion`} component={Atencion} />
+        <Route exact path={`${url}/atenciones-pendientes`} component={AtencionesPendientesPago} />
         <Route exact path={`${url}/not-allowed`} component={NotAllowed} />
         <Redirect to={`${url}/dash`} />
       </Switch>
