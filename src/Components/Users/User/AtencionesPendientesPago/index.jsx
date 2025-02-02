daa985e71134f1be36eac3246603beb1082f1400
maurojjzz <maurojim123@gmail.com>
2025-02-01 22:24:37 -0300

    fix redux on atenciones pendiente pago

--- src/Components/Users/User/AtencionesPendientesPago/index.jsx
+++ src/Components/Users/User/AtencionesPendientesPago/index.jsx
@@ -1,49 +1,39 @@
-import React, { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAtenciones } from "../../../../redux/atenciones/thunks.js";
+
 import styles from "./AtencionesPendientesPago.css";
 
 const AtencionesPendientesPago = () => {
-  const [atencionesPendientes, setAtencionesPendientes] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const { atenciones, pending, error } = useSelector((state) => state.atenciones);
+
 
   useEffect(() => {
-    const fetchAtencionesPendientes = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch("/api/atenciones-pendientes");
-        if (!response.ok) {
-          throw new Error("Error al obtener las atenciones pendientes.");
-        }
-        const data = await response.json();
-        setAtencionesPendientes(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+    dispatch(getAtenciones());
+  }, [dispatch]);
 
-    fetchAtencionesPendientes();
-  }, []);
-
-  if (loading) return <p>Cargando atenciones pendientes de pago...</p>;
+  if (pending) return <p>Cargando atenciones pendientes de pago...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <div className={styles.container}>
       <h1>Atenciones Pendientes de Pago</h1>
-      {atencionesPendientes.length > 0 ? (
+      {atenciones.length > 0 ? (
         <ul className={styles.list}>
-          {atencionesPendientes.map((atencion) => (
+          {console.log(atenciones)}
+          {atenciones?.map((atencion) => (
             <li key={atencion.id} className={styles.item}>
               <p>
-                <strong>Fecha:</strong> {atencion.fecha}
+                <strong>Fecha:</strong> {atencion.fecha_hora_atencion}
               </p>
               <p>
-                <strong>Mascota:</strong> {atencion.mascota}
+                <strong>Mascota:</strong> {atencion.mascota.nombre}
               </p>
               <p>
-                <strong>Monto:</strong> {atencion.monto}
+                <strong>Monto:</strong> {atencion.importe}
               </p>
             </li>
           ))}
