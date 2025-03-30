a65f816455588b9f90a942f2755d31114216aeb1
maurojjzz <maurojim123@gmail.com>
2025-03-29 21:55:19 -0300

    delete commented

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.spec.js",  // Asegúrate de que la ruta coincida con la estructura de tu proyecto
+    specPattern: "cypress/e2e/**/*.spec.js",  
     setupNodeEvents(on, config) {
-      // implement node event listeners here si los necesitas
     },
   },
 });
